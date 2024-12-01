c8a70ee54ffaf7bd60a03171190c2f690c230d12
Kyle Cochrane <kylepc@umich.edu>
2024-12-01 18:43:38 -0500

    optimization for part 2

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+import timeit
 import typing
 
 
@@ -5,6 +6,17 @@ def part_one(left: typing.List[int], right: typing.List[int]) -> int:
     merged = zip(left, right)
     distance = map(lambda x: abs(x[0] - x[1]), merged)
     return sum(distance)
+
+
+def part_two_faster(left: typing.List[int], right: typing.List[int]) -> int:
+    r_count: typing.Dict[int, int] = {}
+    for r in right:
+        if r in r_count:
+            r_count[r] += 1
+        else:
+            r_count[r] = 1
+
+    return sum(map(lambda l: l * r_count.get(l, 0), left))
 
 
 def part_two(left: typing.List[int], right: typing.List[int]) -> int:
@@ -22,7 +34,11 @@ def main():
     left = sorted(map(lambda x: int(x[0]), lines))
     right = sorted(map(lambda x: int(x[1]), lines))
     print(part_one(left, right))
-    print(part_two(left, right))
+    print(part_two_faster(left, right))
+
+    # original took 5.3 seconds, faster took 0.01 seconds
+    # print(f'original: {timeit.timeit(lambda: part_two(left, right), globals=globals(), number=100)}')
+    # print(f'faster: {timeit.timeit(lambda: part_two_faster(left, right), globals=globals(), number=100)}')
 
 
 if __name__ == '__main__':
