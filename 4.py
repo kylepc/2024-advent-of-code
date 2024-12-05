8ceb5dd1d96a3f9081c59bf8ec47f14268ee3ef1
Kyle Cochrane <kylepc@umich.edu>
2024-12-04 23:16:03 -0500

    part two

--- 4.py
+++ 4.py
@@ -41,10 +41,32 @@ def part_one(data: List[str]) -> int:
     return count
 
 
+def part_two(data: List[str]) -> int:
+    def mas(it: Iterable) -> bool:
+        return all(itertools.starmap(operator.eq, zip(it, 'MAS', strict=True))) or \
+            all(itertools.starmap(operator.eq, zip(it, 'SAM', strict=True)))
+
+    count = 0
+    for i, row in enumerate(data):
+        for j, char in enumerate(row):
+            if char == 'A':
+                diag_one = [data[i - 1][j - 1] if i > 0 and j > 0 else '',
+                            'A',
+                            data[i + 1][j + 1] if i < len(data) - 1 and j < len(row) - 1 else '']
+                diag_two = [data[i - 1][j + 1] if i > 0 and j < len(row) - 1 else '',
+                            'A',
+                            data[i + 1][j - 1] if i < len(data) - 1 and j > 0 else '']
+
+                if mas(diag_one) and mas(diag_two):
+                    count += 1
+    return count
+
+
 def main():
     with open('4_input.txt', 'r') as f:
         data = [line.strip('\n') for line in f.readlines()]
     print(part_one(data))
+    print(part_two(data))
 
 
 if __name__ == '__main__':
