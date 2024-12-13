34b7cff1aa48fdbd2d5ba3166633bb3cfb94da97
Kyle Cochrane <kylepc@umich.edu>
2024-12-12 22:35:45 -0500

    part 2

--- 7.py
+++ 7.py
@@ -1,15 +1,31 @@
 from typing import List
 
 
-def is_valid(test: int, numbers: List[int]) -> bool:
+def is_valid(test: int, numbers: List[int], cat: bool = False) -> bool:
     if len(numbers) == 1:
         return test == numbers[0]
-    elif is_valid(test, [numbers[0] + numbers[1]] + numbers[2:]):
+    elif is_valid(test, [numbers[0] + numbers[1]] + numbers[2:], cat):
         return True
-    elif is_valid(test, [numbers[0] * numbers[1]] + numbers[2:]):
+    elif is_valid(test, [numbers[0] * numbers[1]] + numbers[2:], cat):
+        return True
+    elif cat and is_valid(test, [int(str(numbers[0]) + str(numbers[1]))] + numbers[2:], cat):
         return True
     else:
         return False
+
+
+def part_1(equations):
+    result = 0
+    for eq in equations:
+        test = eq[0]
+        numbers = eq[1]
+        if is_valid(test, numbers, False):
+            result += test
+    return result
+
+
+def part_2(equations):
+    return sum(map(lambda eq: eq[0] if is_valid(eq[0], eq[1], True) else 0, equations))
 
 
 def main():
@@ -19,14 +35,8 @@ def main():
     equations = [(int(x[0]), list(map(int, x[1].split(' ')))) for x in split_lines]
     assert len(lines) == len(equations)
 
-    result = 0
-    for eq in equations:
-        test = eq[0]
-        numbers = eq[1]
-        if is_valid(test, numbers):
-            result += test
-
-    print(result)
+    print(part_1(equations))
+    print(part_2(equations))
 
 
 if __name__ == '__main__':
